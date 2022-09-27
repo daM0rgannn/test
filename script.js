f296645602ea79ad2a81e858824152f0b7857323
Phannita Dew <104006857+daM0rgannn@users.noreply.github.com>
2022-09-27 19:59:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// fetch('example_data.json')
+//     .then(function (response) {
+//         return response.json()
+//     })
+//     .then(function (data) {
+//         appendData(data)
+//     })
+//     .catch(function (err) {
+//         console.log('error: ' + err)
+//     })
+
+// function appendData(data) {
+//     var mainContainer = document.getElementById('restaurent');
+//     for (var i = 0; i < data.length; i++){
+//         var div = document.createElement("div");
+//         div.innerHTML = data[i].name + data[i].rating
+//         mainContainer.appendChild(div);
+//     }
+// }
+
